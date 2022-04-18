8835ae92f36e6e8d48a5dc8b17730c56695862ba
Asa Gold <gold1@stolaf.edu>
2022-04-18 18:48:21 -0500

    advisory, start para

--- code/5_viz_reporting.R
+++ code/5_viz_reporting.R
@@ -1,4 +1,12 @@
 library(tidyverse)
+library(RColorBrewer)
+library(showtext)
+library(sysfonts)
+
+# load fonts
+font_add_google("Source Sans Pro")
+
+showtext_auto()
 
 
 #### Reporting Structure ####
@@ -14,10 +22,81 @@ steps_data <- all_list$single$Q3 |>
   mutate(Q3 = stringr::str_remove(Q3, "[:blank:]\\(.+\\)"))
 
 
+#### Advisory Boards ####
 
-#### Student Staff ####
+all_list$multi$Q4 |>
+  
+  dplyr::filter(n>1) |>
+  
+  arrange(-n) |>
+  
+  mutate(freq = format(round(freq,2), nsmall = 2)) |>
+  
+  mutate(value = stringr::str_replace(value, "^My career.+boards$","No Advisory Boards"),
+         value = stringr::str_replace(value, "^We report.+reps$","Academic Programs & Standards Committee")) |>
+  
+  serviceTab(q = "Q4", title = "Advisory Boards",
+             subtitle = "Subtitle",
+             offer = "To Whom Do You Report?")
 
-key <- keyFunction('Q6', dim1,dim2)
+
+
+
+
+#### Performance Metrics ####
+
+rank_data <- question_list$Q5 |>
+  
+  dplyr::filter(dplyr::if_any(.cols = !(1:2), .fns = ~ !is.na(.x))) |>
+  
+  tidyr::pivot_longer(
+    cols = !(1:2),
+    names_to = "Question",
+    values_to = "ranking"
+  ) |>
+  
+  dplyr::left_join(keyFunction('Q5', dim1)) |>
+  
+  dplyr::left_join(all_list$ranking$Q5) |>
+  
+  dplyr::filter(Question != "Q5_13_TEXT") |>
+  
+  dplyr::mutate(ranking = as.numeric(ranking),
+                dim1 = stringr::str_remove(dim1, "[:blank:]\\(.+\\)"))
+
+
+
+
+rank_data |>
+  ggplot(aes(reorder(dim1,-ranking_avg), ranking, colour = ranking))+
+  geom_vline(xintercept = rank_data$dim1, colour = "grey")+
+  geom_jitter(alpha = 0.5, width = 0, size = 3)+
+  # geom_point(aes(reorder(dim1,-ranking_avg), ranking_avg), 
+  #           colour = "grey",
+  #           size = 5)+
+  coord_flip()+
+  scale_y_reverse(breaks = seq(1,11))+
+  scale_color_gradientn(colours = c('chartreuse4','grey','hotpink2'))+
+  
+  labs(
+    title = "Performance Metrics: Importance Ranking",
+    subtitle = "Subtitle (n = n_respond)",
+    y = "Importance Ranking\n (1=highest)"
+  )+
+  theme(
+    panel.background = element_blank(),
+    text = element_text(family = "Source Sans Pro", size = 15),
+    axis.ticks.y = element_blank(),
+    axis.title.y = element_blank(),
+    axis.title.x = element_text(size = 13),
+    legend.position = "none"
+  )
+
+
+
+
+
+#### Student Staff (Total) ####
 
 student_staff_data <- question_list[['Q6']] |>
   tidyr::pivot_longer(
@@ -25,12 +104,32 @@ student_staff_data <- question_list[['Q6']] |>
     names_to = "Question",
     values_to = "response"
   ) |>
-  dplyr::left_join(key) |>
+  dplyr::left_join(
+    keyFunction('Q6', dim1,dim2)
+    ) |>
   dplyr::mutate(response = as.numeric(response)) |>
   dplyr::group_by(`Institution Name`) |>
   dplyr::summarise(n = sum(response, na.rm = TRUE)) |>
   dplyr::filter(n > 0 & !is.na(n))
 
+
+
+#### Student Staff (Paraprofessional) ####
+
+student_para_data <- question_list[['Q6']] |>
+  tidyr::pivot_longer(
+    cols = !(1:2),
+    names_to = "Question",
+    values_to = "response"
+  ) |>
+  dplyr::left_join(
+    keyFunction('Q6', dim1,dim2)
+  ) |>
+  dplyr::mutate(response = as.numeric(response)) |>
+  dplyr::filter(dim1 == "Students in paraprofessional roles") |>
+  dplyr::group_by(`Institution Name`) |>
+  dplyr::summarise(n = sum(response, na.rm = TRUE)) |>
+  dplyr::filter(n > 0 & !is.na(n))
 
 
 #### Professional Staff ####
