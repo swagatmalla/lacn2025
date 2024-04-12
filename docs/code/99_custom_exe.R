2a33fcb0cd6ecd71560cefdb23b7041c93857335
Swagat <97408976+swagatmalla@users.noreply.github.com>
2024-04-12 15:44:34 -0500

    fixed typos

--- docs/code/99_custom_exe.R
+++ docs/code/99_custom_exe.R
@@ -1,13 +1,36 @@
+library(purrr)
+library(glue)
+library(stringr)
+library(here)
+
+# Load the data
 load("lacn.RData")
 
+# Extract the college names from the loaded data
 colleges <- as.list(question_list$Q1$`Institution Name`)
 
+# Define a function to render R Markdown and manage file connections
+render_custom_template <- function(college) {
+  # Define output file path based on college name
+  output_file <- file.path(here::here(), "docs/custom", glue("{str_remove_all(college, '[:blank:]')}.html"))
+  
+  # Open file connection
+  file_con <- file(output_file, "w")
+  
+  # Render R Markdown document
+  rmarkdown::render(
+    input = "docs/custom_template.Rmd",
+    params = list(college = college),
+    output_file = file_con,
+    clean = TRUE  # Clean intermediate files after rendering
+  )
+  
+  # Close file connection
+  close(file_con)
+}
+
+# Use purrr::walk() to iterate through colleges and render R Markdown
 purrr::walk(
   .x = colleges,
-  ~ rmarkdown::render(
-    input = "docs/custom_template.Rmd",
-    params = list(college = {.x}),
-    output_file = file.path(here::here(),"docs/custom",glue::glue("{str_remove_all(.x,\"[:blank:]\")}.html")),
-  )
+  ~ render_custom_template(.x)
 )
-
