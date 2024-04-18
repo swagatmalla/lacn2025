3c4f4bb9da033220fbe61ace22d8baa2bec990c9
Swagat <97408976+swagatmalla@users.noreply.github.com>
2024-04-18 14:23:29 -0500

    custom reports completed except for Smith and Amherst

--- docs/code/99_custom_exe.R
+++ docs/code/99_custom_exe.R
@@ -1,36 +1,14 @@
-library(purrr)
-library(glue)
-library(stringr)
-library(here)
-
-# Load the data
 load("lacn.RData")
 
-# Extract the college names from the loaded data
-colleges <- as.list(question_list$Q1$`Institution Name`)
+colleges <- as.list(question_list$Q1$`Institution Name`)[36:40]
 
-# Define a function to render R Markdown and manage file connections
-render_custom_template <- function(college) {
-  # Define output file path based on college name
-  output_file <- file.path(here::here(), "docs/custom", glue("{str_remove_all(college, '[:blank:]')}.html"))
-  
-  # Open file connection
-  file_con <- file(output_file, "w")
-  
-  # Render R Markdown document
-  rmarkdown::render(
-    input = "docs/custom_template.Rmd",
-    params = list(college = college),
-    output_file = file_con,
-    clean = TRUE  # Clean intermediate files after rendering
-  )
-  
-  # Close file connection
-  close(file_con)
-}
-
-# Use purrr::walk() to iterate through colleges and render R Markdown
 purrr::walk(
   .x = colleges,
-  ~ render_custom_template(.x)
+  ~ rmarkdown::render(
+    input = "docs/custom_template.Rmd",
+    params = list(college = {.x}),
+    output_file = file.path(here::here(),"docs/custom",glue::glue("{str_remove_all(.x,\"[:blank:]\")}.html")),
+    clean = TRUE
+  ) 
 )
+
